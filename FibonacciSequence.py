0d7d05a9cfa792a821041ae4102addb83977bcc3
r-ghv <raghavendraashokk@gmail.com>
2024-06-06 21:23:00 +0530

    Update FibonacciSequence.py

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 #This is a program to print the fibonacci sequence upto nth term
 fib = [0, 1]
 n = int(input("Enter the number of terms to be displayed in the Fibonacci sequence:"))
@@ -12,8 +6,6 @@ for i in range(2,n):
 for i in range(n):
     print(fib[i])
 
-
-# In[ ]:
 
 
 
