bee23a6c427d7c3d28c2badc36441cea2c656824
r-ghv <raghavendraashokk@gmail.com>
2024-06-06 21:43:58 +0530

    Add files via upload

---
+++ PosNumbers.py
@@ -0,0 +1,10 @@
+#this is a program to print a sublist of positive integers from a list of integers.
+list = input("Enter a list of integers seperated by a space:").split()
+plist = [int(num) for num in list]
+pos_list = [num for num in plist if num > 0]
+print(f"Input List: {plist}")
+print(f"Output List: {pos_list}")
+
+
+
+
